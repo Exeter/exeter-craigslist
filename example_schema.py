e7d09fb20eafed3d9a37b63f91a1b0ec90560dc8
dabbler0 <dab1998@gmail.com>
2013-10-19 15:33:07 -0400

    Update example to fit current schema

--- example_schema.py
+++ example_schema.py
@@ -22,9 +22,12 @@ class Connection:
 
     # Ensure that the tables exist
     c.executescript("""
-      CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY ASC, email TEXT, sesskey TEXT, bookmarks TEXT);
-      CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY ASC, author TEXT, created INTEGER, edited INTEGER, title TEXT, body TEXT, tag TEXT, image BLOB, bookmarked TEXT);
-      CREATE TABLE IF NOT EXISTS tags (id INTEGER PRIMARY KEY ASC, tag TEXT, tagged TEXT);
+      CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY ASC, email TEXT, sesskey TEXT);
+      CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY ASC, category TEXT, author TEXT, created INTEGER, refresh INTEGER, title TEXT, body TEXT, tag TEXT, image BLOB, flagged TEXT, unlink INTEGER);
+      CREATE INDEX refresh_index ON posts(refresh);
+      CREATE INDEX created_index ON posts(created);
+      CREATE INDEX category_index ON posts(category);
+      CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY ASC, name TEXT, posts TEXT);
     """)
     
     # Commit the table creation
@@ -48,7 +51,7 @@ class Connection:
 
     else:
       # Otherwise, create an empty user record and set its session key
-      self.c.execute("INSERT INTO users (email, sesskey, bookmarks) VALUES (?, ?, ?)", (email, sesskey, "[]"))
+      self.c.execute("INSERT INTO users (email, sesskey) VALUES (?, ?, ?)", (email, sesskey))
     
     # Commit this database change.
     self.conn.commit()
@@ -66,46 +69,53 @@ class Connection:
     self.c.execute("SELECT sesskey FROM users WHERE email=?", (email,))
     return (self.c.fetchone()[0] == sesskey)
 
-  def bookmark(self, email, post):
-    # Get this user's current bookmarks
-    self.c.execute("SELECT bookmarks FROM users WHERE email=?", (email,))
-    current_bookmarks = json.loads(self.c.fetchone()[0])
-
-    # Update it
-    current_bookmarks.append(post)
-    self.c.execute("UPDATE users WHERE email=? SET bookmarks=?", (json.dumps(current_bookmarks)))
-
-    # Get this post's current bookmarkers
-    self.c.execute("SELECT bookmarkers FROM users WHERE email=?", (email,))
+  def flag(self, email, post):
+    # Get this post's current flaggers
+    self.c.execute("SELECT flags FROM posts WHERE email=?", (email,))
     current_bookmarkers = json.loads(self.c.fetchone()[0])
 
-    # Update them
-    current_bookmarkers.append(email)
-    self.c.execute("UPDATE users WHERE email=? SET bookmarkers=?", (json.dumps(current_bookmarkers),))
+    # If we haven't already flagged this yet with this users, do so
+    if email not in current_bookmarkers:
+      current_bookmarkers.append(email)
+      self.c.execute("UPDATE posts SET flags=?", (json.dumps(json.dumps(current_bookmarkers)),))
 
     # Commit
     self.conn.commit()
 
-  def get_posts_by_creation(self, timestamp, content_search, tag_search):
+  def get_posts(self, timestamp, limit, content_search=None, category=None):
     # Fetch all matching posts
-    self.c.execute("SELECT * FROM posts WHERE created>? AND body LIKE ? AND tag LIKE ? ORDER BY created DESC LIMIT 10", (timestamp, "%%%s%%" % content_search, "%%%s%%" % tag_search))
-    return self.c.fetchall()
+    if category is not None:
+      self.c.execute("SELECT * FROM posts WHERE refresh>? AND unlink!=1 ORDER BY refresh DESC") 
+    else:
+      self.c.execute("SELECT * FROM posts WHERE refersh>? AND unlink!= AND category=? ORDER BY refresh DESC", (category,))
 
-  def get_posts_by_edition(self, timestamp, content_search, tag_search):
-    # Fetch all matching posts
-    self.c.execute("SELECT * FROM posts WHERE edited>? AND body LIKE ? AND tag LIKE ? ORDER BY edited DESC LIMIT 10", (timestamp, "%%%s%%" % content_search, "%%%s%%" % tag_search))
-    return self.c.fetchall()
+    if content_search is not None:
+      # If we're asked to execute a search, do so
+      produced_rows = []
+
+      # Iterate over the rows to find the results
+      for row in self.c:
+        # If this result matches, append it
+        produced_rows.append(row) if content_search in row[5] else 0
+
+        # If we have enough results, return
+        if len(produced_rows) > limit:
+          return produced_rows
+
+    else:
+      # Otherwise, return (limit) rows
+      return (self.c.fetchone() for _ in range(0, limit))
   
   def post(self, author, title, body, tag, image):
     # Create this post
-    self.c.execute("INSERT INTO posts (author, created, edited, title, body, tag, image, bookmarked) VALUES (?, ?, ?, ?, ?, ?, ?, '[]')", author, time.time(), time.time(), title, body, tag, image)
+    self.c.execute("INSERT INTO posts (author, created, refresh, title, body, tag, image, flags, unlink) VALUES (?, ?, ?, ?, ?, ?, ?, '[]', 0)", author, time.time(), time.time(), title, body, tag, image, )
     
     # Commit
     self.conn.commit()
 
   def edit_post(self, post_id, title, body, tag, image):
     # Update this post
-    self.c.execute("UPDATE posts SET title=?, body=?, tag=?, image=?, edited=? WHERE id=?", (title, body, tag, image, time.time(), post_id))
+    self.c.execute("UPDATE posts SET title=?, body=?, tag=?, image=?, refresh=? WHERE id=?", (title, body, tag, image, time.time(), post_id))
   
     # Commit
     self.conn.commit()
@@ -114,6 +124,16 @@ class Connection:
     # Do any last-minute commits and then close
     self.conn.commit()
     self.conn.close()
+
+  def garbage_collect(self):
+    # Issue the command to grab all the rows
+    self.c.execute("SELECT id, flags FROM posts")
+    
+    # Iterate over the rows
+    for row in self.c:
+      if len(json.loads(row[1])) > FLAG_UNLINK_THRESHOLD:
+        # If a post has been flagged too many times, delete it.
+        self.c.execute("UPDATE posts SET unlink=1 WHERE posts=?")
 
   def __del__(self):
     self.close()
