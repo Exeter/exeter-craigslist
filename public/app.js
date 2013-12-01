256446bd809028f19637d742ba0ca28746be2f9b
Sean Lee <freshdried@gmail.com>
2013-12-01 01:42:32 -0500

    Redesigned some css, fully implemented client-side routing

--- public/app.js
+++ public/app.js
@@ -5,100 +5,104 @@
 var TESTING = (function(){
 	var posts = [
 		{
-			id: '000000000',
+			id: 11,
 			title: 'Title',
 			description: "This is my Title! Wanna buy it!",
 			date: "2013-10-14T02:53:50.322Z",
 			category: 'furniture',
-			//image:
+			imageurl: "http://info.infiniteconferencing.com/Portals/98866/images/catchy%20titles-resized-600.png"
 		},
 		{
-			id: '000000001',
+			id: 12,
 			title: 'Tittle',
 			description: "This is a tittle!",
 			date: "2013-10-15T02:53:50.322Z",
 			price: "$40",
 			category: 'furniture',
-			//image:
+    			imageurl: "http://www.daniellebaird.net/blog/wp-content/uploads/2010/12/tittle-typography-01.jpg"
 		},
 		{
+			id: 13,
 			title: 'Titttle',
 			description: "This is a titttle!",
 			date: "2013-10-16T02:53:50.322Z",
 			price: "$40",
-			category: 'textbooks'
-			//image:
+			category: 'textbooks',
+			imageurl: "http://static.fjcdn.com/pictures/Unique+titttle_11423b_3511365.jpg"
 		},
 		{
+			id: 14,
 			title: 'physics textbook',
 			description: "this reminds me of the worst year of my life, but nothing is written in it and as a matter of fact, it's never really been opened. #physics #textbook #advanced",
 			date: "2013-10-16T02:53:50.322Z",
 			price: "$100",
-			category: 'textbooks'
+			category: 'textbooks',
+			imageurl: "http://www.hwscience.com/physics/apphysicsc/SJ%208th%20Ed.jpg"
+
 		},
 		{
+			id: 15,
 			title: 'greek dictionary',
 			description: "by definition, the most useless thing in this century",
 			date: "2013-10-16T02:53:50.322Z",
 			price: "$100",
-			category: 'textbooks'
-			//image: 
+			category: 'textbooks',
+			imageurl: "http://bibliagora.co.uk/images/P/etymologiko_babiniotis.jpg"
 		},
 		{
-			title: 'greek dictionary',
-			description: "by definition, the most useless thing in this century",
-			date: "2013-10-16T02:53:50.322Z",
-			price: "$100",
-			category: 'textbooks'
-			//image: 
-		},
-		{
-			title: 'TI-84',
+			id: 17,
+			title: 'Extra TI-84',
 			description: "so old you can donate it to a museum. prices according to a priceless relic #calculator #ti84",
 			date: "2013-10-16T02:53:50.322Z",
 			price: "$90",
-			category: 'electronics'
-			//image: 
+			category: 'electronics',
+			imageurl: "http://www.ticalc.org/images/calcs/84plus-big.gif"
 		},
 		{
-			title: 'TI-84',
+			id: 18,
+			title: 'TI-84 Plus Silver Edition for sale',
 			description: "don't want it anymore because it is useless for math + science here...but you should buy it anyways! ps missing all its keys pps bateries not included ppps neither is the screen #ti84 #calculator",
 			date: "2013-10-16T02:53:50.322Z",
 			price: "$200",
-			category: 'electronics'
-			//image: 
+			category: 'electronics',
+			imageurl: 'http://www.ticalc.org/images/calcs/84plus-se-big.gif'
 		},
 		{
-			title: 'graph paper',
+			id: 19,
+			title: 'loads of graph paper',
 			description: "graph paper (used) but it's good for scratch work i guess #graphpaper",
 			date: "2013-10-16T02:53:50.322Z",
 			price: "$5",
-			category: 'supplies'
-			//image: 
+			category: 'supplies',
+			imageurl: 'http://www.northwestern.edu/msa/media/images/stack_of_papers.gif'
 		},
 		{
-			title: 'hammock',
+			id: 110,
+			title: 'nice hammock',
 			description: "good for spring term. napping. #hammock #furniture ",
 			date: "2013-10-16T02:53:50.322Z",
 			price: "$15",
-			category: 'supplies'
-			//image: 
+			category: 'supplies',
+			imageurl: 'http://www.irunoninsulin.com/wp-content/uploads/2011/11/hammock-between-two-palm-trees-fla517.jpg'
 		},
 		{
-			title: 'pencil',
+			id: 111,
+			title: 'chewed on pencil -- will deliver',
 			description: "warning: i chewed on it, but i cleaned it with hand sanitizer, so it should all be ok. (caution: 2 inches left, ready for compost) #pencil",
 			date: "2013-10-16T02:53:50.322Z",
 			price: "$1",
-			category: 'supplies'
-			//image: 
+			category: 'supplies',
+			imageurl: 'http://www.acefitness.org/blogs/images/posts/16/chewedpencil.jpg'
+
 		},
 		{
-			title: 'leftovers',
+			id: 112,
+			title: 'leftover food from olas!',
 			description: "las olas leftovers...don't know what it is exactly, but it's green, squishy, and smells like fine cheese. #food #lasolas",
 			date: "2013-10-16T02:53:50.322Z",
 			price: "$0",
-			category: 'other'
-			//image: 
+			category: 'other',
+			imageurl: 'http://hbh.halfbakedharvest.netdna-cdn.com/wp-content/uploads/2013/04/Chipotle-Steak-Burrito-41.jpg'
 		}
 	];
 
@@ -162,15 +166,9 @@ $(document).ready(function() {(function(MODE) {
 		render: function() {
 			this.$el.html($("#login-template").html());
 		},
-	    	render_fail: (function() {
-			var failed = false;
-			return function() {
-				if (!failed) {
-					$("#login-form").prepend('<i>try again...');
-					failed = true;
-				}
-			}
-		})(),
+	    	render_fail: function() {
+			$('#login_password').val('').attr('placeholder', 'try again...');
+		},
 	});
 
 	var AuthedView = Backbone.View.extend({
@@ -202,6 +200,12 @@ $(document).ready(function() {(function(MODE) {
 
 //Content Stuff
 	var contentController = new (function() {
+		var current_post = null;
+		this.switch_post = function(id) {
+			current_post = posts.get(id);
+		}
+		var _contentController = this;
+
 		var views = {
 			PostsView: Backbone.View.extend({
 				el: '#content',
@@ -214,14 +218,15 @@ $(document).ready(function() {(function(MODE) {
 						ListPostsView: Backbone.View.extend({
 							el: "#posts-content",
 							render: function() {
-								var html = "<ul>";
+								var list = $("<ul></ul>");
+
 								var that = this;
 								posts.each(function(post) {
-									var element = "<li>" + post.get("title") + "</li>";
-									html += element;
+									var element = $("<li>" + post.get("title") + "</li>");
+									element.click(function() { router.navigate("viewpost/" + post.id, {trigger: true}); });
+									list.append(element);
 								});
-							       	html += "</ul>";
-								this.$el.html(html);
+								this.$el.html(list);
 							}
 						}),
 						GridPostsView: Backbone.View.extend({
@@ -258,6 +263,18 @@ $(document).ready(function() {(function(MODE) {
 			}),
 
 
+			PostView: Backbone.View.extend({
+				el: '#content',
+				template: _.template($("#post-template").html()),
+				render: function() {
+					this.model = current_post;
+					var html = this.model.get("description");
+					this.$el.html(this.template(this.model.attributes));
+
+				},
+			}),
+
+
 			NewPostView: Backbone.View.extend({
 				el: '#content',
 				render: function() {
@@ -273,6 +290,7 @@ $(document).ready(function() {(function(MODE) {
 				}
 			})
 		};
+
 
 		this.show = function(viewstring) {
 			if (this.currentView){
@@ -295,6 +313,7 @@ $(document).ready(function() {(function(MODE) {
 	    		'': "index", 
 			"newpost": "newpost",
 			"myposts": "myposts",
+	    		"viewpost/:id": "viewpost",
 	    		"*notFound": 'notFound',
 		},
 	    	index: function() {
@@ -306,8 +325,12 @@ $(document).ready(function() {(function(MODE) {
 	    	myposts: function() {
 			contentController.show('MyPostsView');
 		},
+	    	viewpost: function(id) {
+			contentController.switch_post(id);
+			contentController.show('PostView');
+		},
 	    	notFound: function() {
-			console.log("not found...");
+			$("#content").html("page not found!");
 		},
 	});
 	router = new Router;
